88717a7c4a0184ac805f94649d85b3719a50f138
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-07-09 00:39:22 +0900

    fix(anti pattern): delete Capture magic number

--- src/features/capture/components/Capture.tsx
+++ src/features/capture/components/Capture.tsx
@@ -9,14 +9,20 @@ import { TouchableOpacity, View } from "react-native";
 
 const AnimatedCameraView = Animated.createAnimatedComponent(CameraView);
 
+const ZOOM_VALUE = {
+  min: 0,
+  max: 1,
+  speed: 0.02,
+} as const;
+
 const Capture = () => {
   const { cameraRef, takePhoto } = useCamera();
-  const zoom = useSharedValue(0);
+  const zoom = useSharedValue<number>(ZOOM_VALUE.min);
 
   const pinchGesture = Gesture.Pinch().onUpdate((event) => {
     zoom.value = Math.min(
-      Math.max(0, zoom.value + (event.scale - 1) * 0.02),
-      1
+      Math.max(0, zoom.value + (event.scale - 1) * ZOOM_VALUE.speed),
+      ZOOM_VALUE.max
     );
   });
 
