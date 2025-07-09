af1d63632da46155f2ef4aed79a67ca0e0a297d0
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-07-09 22:03:03 +0900

    refactor: usePickLibrary내부 permission 분리

--- src/features/capture/hooks/usePickLibrary.ts
+++ src/features/capture/hooks/usePickLibrary.ts
@@ -2,18 +2,23 @@ import { useURIContext } from "../context";
 import * as ImagePicker from "expo-image-picker";
 import { Alert } from "react-native";
 
-const usePickLibrary = () => {
+const checkLibraryPremission = async () => {
+  const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+  if (!granted) {
+    Alert.alert("권한 필요", "사진 접근 권한이 필요합니다.");
+  }
+
+  return granted;
+};
+
+const usePickLibrary = async () => {
+  const libraryGranted = await checkLibraryPremission();
+  if (!libraryGranted) return;
+
   const { setPhotoURI } = useURIContext();
 
   const pickPicture = async () => {
-    const libraryPremissionResult =
-      await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-    if (!libraryPremissionResult.granted) {
-      Alert.alert("권한 필요", "사진 접근 권한이 필요합니다.");
-      return;
-    }
-
     const pickedPicture = await ImagePicker.launchImageLibraryAsync({
       selectionLimit: 1,
       mediaTypes: "images",
