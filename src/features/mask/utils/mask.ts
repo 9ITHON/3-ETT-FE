1240dc83387f06cbcffbe124229923e5d402eb82
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-07-11 05:17:36 +0900

    refactor(mask): mask 함수 분리, 선언적으로 작성

--- src/features/mask/utils/mask.ts
+++ src/features/mask/utils/mask.ts
@@ -5,22 +5,32 @@ interface MaskResult {
   maskedText: string;
   PIIMap: Map<string, string>;
 }
+
+const isNameGroupValid = (type: PIIType, groups: string[]) => {
+  return type === "이름" && groups[1];
+};
+
+const replacePII = (
+  text: string,
+  type: PIIType,
+  regexp: RegExp,
+  mapper: PIIMapper
+): string => {
+  return text.replace(regexp, (...args) => {
+    const fullPIIText = args[0];
+    const PIIGroups = args.slice(1, -2); // -2: match start index
+    const PII = isNameGroupValid(type, PIIGroups) ? PIIGroups[1] : fullPIIText;
+    const maskedPII = mapper.getMaskedPII(type, PII);
+
+    return fullPIIText.replace(PII, maskedPII);
+  });
+};
+
 const mask = (text: string): MaskResult => {
   const mapper = new PIIMapper();
-  let maskedText = text;
-
-  for (const type of Object.keys(PIIRegExp) as PIIType[]) {
-    const _PIIRegExp = PIIRegExp[type];
-
-    maskedText = maskedText.replace(_PIIRegExp, (...args) => {
-      const fullPIIText = args[0];
-      const PIIGroups = args.slice(1, -2); // -2: match start index
-      const PII = type === "이름" && PIIGroups[1] ? PIIGroups[1] : fullPIIText;
-      const maskedPII = mapper.getMaskedPII(type, PII);
-
-      return fullPIIText.replace(PII, maskedPII);
-    });
-  }
+  const maskedText = Object.entries(PIIRegExp).reduce((acc, [key, regexp]) => {
+    return replacePII(acc, key as PIIType, regexp, mapper);
+  }, text);
 
   const PIIMap = mapper.getPIIMap();
 
