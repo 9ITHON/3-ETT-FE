488235a22614625843004e08e98a516e7d4fc649
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-07-11 19:16:57 +0900

    fix(translate): translate의 인자로 payload를 정의하여 type 별로 다르게 처리

--- src/features/translate/utils/handleOCRTranslate.ts
+++
@@ -1,9 +0,0 @@
-import { getOcrTexts } from "@/features/ocr/utils";
-import translate from "./translate";
-
-const handleOCRTranslate = async (photoURI: string) => {
-  const ocrText = await getOcrTexts(photoURI);
-  translate(ocrText);
-};
-
-export default handleOCRTranslate;
--- src/features/translate/utils/translate.ts
+++ src/features/translate/utils/translate.ts
@@ -1,8 +1,21 @@
 import { mask, unMask } from "@/features/mask/utils";
+import { TranslatePayload } from "../types";
+import { getOcrTexts } from "@/features/ocr/utils";
 
-const translate = (text: string) => {
+const translate = async (paylaod: TranslatePayload) => {
+  let originText = "";
+
+  if (paylaod.type === "picture") {
+    const ocrText = await getOcrTexts(paylaod.uri);
+    originText = ocrText;
+  }
+
+  if (paylaod.type === "text") {
+    originText = paylaod.text;
+  }
+
   // mask on
-  const { maskedText, PIIMap } = mask(text);
+  const { maskedText, PIIMap } = mask(originText);
 
   // get easy text from server
 
