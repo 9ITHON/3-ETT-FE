3568dd6c8957badfe8948b589a70282594e16a82
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-07-12 00:31:30 +0900

    refactor(translate): status에 따른 컴포넌트를 매핑하는 Map을 생성하여 리턴하도록 변경

--- src/screens/translate/complete/TranslateViewer.tsx
+++ src/screens/translate/complete/TranslateViewer.tsx
@@ -2,7 +2,7 @@ import { RouteProp, useRoute } from "@react-navigation/native";
 import { RootStackParamList } from "@/navigation/Navigation";
 import { SCREEN } from "@/constants/screen";
 import { translate } from "@/features/translate";
-import { useCallback, useEffect, useState } from "react";
+import { JSX, useCallback, useEffect, useState } from "react";
 import {
   TranslateError,
   TranslateLoading,
@@ -14,14 +14,14 @@ type TranslateScreenRouteProp = RouteProp<
   RootStackParamList,
   typeof SCREEN.TranslateViewer
 >;
-type translateStatus = "loading" | "success" | "error";
+type TranslateStatus = "loading" | "success" | "error";
 
 const TranslateViewer = () => {
   const { payload } = useRoute<TranslateScreenRouteProp>().params;
 
   // 번역 상태
   const [translateStatus, setTranslateStatus] =
-    useState<translateStatus>("loading");
+    useState<TranslateStatus>("loading");
 
   // 번역 결과물
   const [easyText, setEasyText] = useState<string | null>(null);
@@ -41,10 +41,15 @@ const TranslateViewer = () => {
     runTranslate();
   }, [runTranslate]);
 
-  if (translateStatus === "loading") return <TranslateLoading />;
-  if (translateStatus === "error")
-    return <TranslateError onRetry={runTranslate} />;
-  return <TranslateSuccess easyText={easyText} onRetry={runTranslate} />;
+  const TranslateStatusMapper: Record<TranslateStatus, JSX.Element> = {
+    loading: <TranslateLoading />,
+    error: <TranslateError onRetry={runTranslate} />,
+    success: (
+      <TranslateSuccess easyText={easyText ?? ""} onRetry={runTranslate} />
+    ),
+  };
+
+  return TranslateStatusMapper[translateStatus];
 };
 
 export default TranslateViewer;
