4f33b6335f45416233f5b74300706a60cf44db61
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-07-13 04:50:13 +0900

    chore: delete log

--- src/features/translate/utils/translate.ts
+++ src/features/translate/utils/translate.ts
@@ -4,26 +4,17 @@ import extractTextByType from "./extractTextByType";
 import getEasyText from "../api/getEasyText";
 
 const translate = async (paylaod: TranslatePayload) => {
-  console.log(paylaod);
-  console.log("⭐⭐⭐");
-
   const originText = await extractTextByType(paylaod);
-  console.log("🔥1: ", originText);
 
-  // mask on
   const { maskedText, PIIMap } = mask(originText);
-  console.log("🔥2: ", maskedText);
 
-  // get easy text from server
   const { translatedText, timestamp } = await getEasyText(maskedText);
+
   const maskedEasyText = translatedText;
 
-  // mask off
   const unMaskedText = unMask({ maskedEasyText, PIIMap });
-  console.log("🔥4: ", unMaskedText);
 
   const easyText = unMaskedText;
-  console.log("🔥5: ", easyText);
 
   return {
     easyText,
